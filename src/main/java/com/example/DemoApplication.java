916abb6391fdbcd0da9febc215c17b5a31f401db
yohann555 <richardyohann@free.fr>
2017-09-28 13:58:19 +0200

    new

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -1,22 +1,12 @@
 package com.example;
 
-import org.junit.Test;
-import static org.junit.Assert.assertTrue;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
-public class DemoApplicationTests {
+@SpringBootApplication
+public class DemoApplication {
 
-        @Test
-        public void contextLoads() {
-        }
-
-        @Test
-        public void myTest(){
-assertTrue(true);
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
 }
-}
-
