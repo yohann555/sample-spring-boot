7f071e7af97188c4335beb5814658163e2e35a00
yohann555 <richardyohann@free.fr>
2017-09-29 10:36:28 +0200

    re

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -9,4 +9,12 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+
+    public static Integer additionner(Integer a, Integer b) throws Exception {
+if (a != null && b != null){
+return a+b;
+}
+throw new Exception();
+}
+
 }
